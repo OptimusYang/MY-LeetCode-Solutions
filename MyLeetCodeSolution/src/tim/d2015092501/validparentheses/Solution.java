35922645def38acb666dc0d535760f4348a8cb42
Tim <nullpointerex@yeah.net>
2015-09-25 17:50:27 +0800

    format change

--- MyLeetCodeSolution/src/tim/d2015092501/validparentheses/Solution.java
+++ MyLeetCodeSolution/src/tim/d2015092501/validparentheses/Solution.java
@@ -6,67 +6,71 @@ public class Solution {
 	/**
 	 * Valid Parentheses
 	 * 
-	 * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-		The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
-		
-		https://leetcode.com/problems/valid-parentheses/
+	 * Given a string containing just the characters '(', ')', '{', '}', '[' and
+	 * ']', determine if the input string is valid.
+	 * 
+	 * The brackets must close in the correct order, "()" and "()[]{}" are all
+	 * valid but "(]" and "([)]" are not.
+	 * 
+	 * https://leetcode.com/problems/valid-parentheses/
+	 * 
 	 * @author Tim
 	 */
 	public static boolean isValid(String s) {
-		if(s == null || s.length() ==0){
+		if (s == null || s.length() == 0) {
 			return true;
 		}
-		
-		if(s.length() == 1){
+
+		if (s.length() == 1) {
 			return false;
 		}
 		Stack<Character> stack = new Stack<Character>();
-        char[] sc = s.toCharArray();
-        boolean valid = true;
-        for(char c : sc){
-        	if(c == '(' || c=='{' || c=='['){
-        		stack.push(c);
-        	}
-        	if(c == ')'){
-        		if(stack.isEmpty()){
-        			valid = false;
-        			break;
-        		}
-        		char cpop = stack.pop();
-        		if(cpop != '('){
-        			valid = false;
-        			break;
-        		}
-        	}
-        	if(c == ']'){
-        		if(stack.isEmpty()){
-        			valid = false;
-        			break;
-        		}
-        		char cpop = stack.pop();
-        		if(cpop != '['){
-        			valid = false;
-        			break;
-        		}
-        	}
-        	if(c == '}'){
-        		if(stack.isEmpty()){
-        			valid = false;
-        			break;
-        		}
-        		char cpop = stack.pop();
-        		if(cpop != '{'){
-        			valid = false;
-        			break;
-        		}
-        	}
-        }
-        if(!stack.isEmpty()){
-        	valid = false;
-        }
-        return valid;
-    }
+		char[] sc = s.toCharArray();
+		boolean valid = true;
+		for (char c : sc) {
+			if (c == '(' || c == '{' || c == '[') {
+				stack.push(c);
+			}
+			if (c == ')') {
+				if (stack.isEmpty()) {
+					valid = false;
+					break;
+				}
+				char cpop = stack.pop();
+				if (cpop != '(') {
+					valid = false;
+					break;
+				}
+			}
+			if (c == ']') {
+				if (stack.isEmpty()) {
+					valid = false;
+					break;
+				}
+				char cpop = stack.pop();
+				if (cpop != '[') {
+					valid = false;
+					break;
+				}
+			}
+			if (c == '}') {
+				if (stack.isEmpty()) {
+					valid = false;
+					break;
+				}
+				char cpop = stack.pop();
+				if (cpop != '{') {
+					valid = false;
+					break;
+				}
+			}
+		}
+		if (!stack.isEmpty()) {
+			valid = false;
+		}
+		return valid;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isValid("(("));
 
