9f0feabf1b1bcbe2892f335596956d6104721bdb
Tim Yang <nullpointerex@yeah.net>
2020-09-12 22:33:14 +0800

    d637 and change to JDK14

---
+++ MyLeetCodeSolution/src/tim/d637/Solution.java
@@ -0,0 +1,58 @@
+package tim.d637;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        //define list for left and right
+        List<TreeNode> ts = new ArrayList<TreeNode>();
+        int level = 0;
+        List<Double> avg = new ArrayList<Double>();
+        avg.add(root.val / 1.0);
+        if (root.left != null) {
+            ts.add(root.left);
+        }
+        if (root.right != null) {
+            ts.add(root.right);
+        }
+        while (ts.size() > 0) {
+            long sum = 0;
+            List<TreeNode> tsSub = new ArrayList<TreeNode>();
+            for (TreeNode tn : ts) {
+                sum += tn.val;
+                if (tn.left != null) {
+                    tsSub.add(tn.left);
+                }
+                if (tn.right != null) {
+                    tsSub.add(tn.right);
+                }
+            }
+            double currentAvg = sum / (ts.size() * 1.0);
+            avg.add(currentAvg);
+            ts = tsSub;
+        }
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2147483647);
+        root.left = new TreeNode(2147483647);
+        root.right = new TreeNode(2147483647);
+        Solution s = new Solution();
+        List<Double> rtn = s.averageOfLevels(root);
+        System.out.println(rtn);
+    }
+}
